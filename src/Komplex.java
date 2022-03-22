0e994e269e9e57478eb97c2d4487732b2dd5f190
albertsparrman <77719327+albertsparrman@users.noreply.github.com>
2022-03-22 15:10:57 +0100

    ???

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,5 +1,24 @@
 public class Komplex {
-    public Komplex(double v, double v1) {
+    double a = 0;
+    double b = 0;
 
+    public Komplex(double re, double im) {
+        this.a = re;
+        this.b = im;
+    }
+
+    public Komplex add(Komplex b) {
+        return
+    }
+
+    public Komplex multiply(Komplex b) {
+    }
+
+    public double arg() {
+    }
+
+    @Override
+    public String toString() {
+        return "Komplex{" + a + "+" + b +"i" + "}";;
     }
 }
